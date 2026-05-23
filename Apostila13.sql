cab9955a35771c11f7ce73b4bcf7cdd97e870889
Nathan Lima <nathanfalima189@gmail.com>
2026-05-23 17:05:28 -0300

    Remove Apostila13.sql

--- Apostila13.sql
+++
@@ -1,8 +0,0 @@
--- 1.1 Adicione uma coluna à tabela tb_pessoa chamada ativo. Ela indica se a pessoa está
--- ativa no sistema ou não. Ela deve ser capaz de armazenar um valor booleano. Por padrão,
--- toda pessoa cadastrada no sistema está ativa. Se necessário, consulte o Link 1.1.1.
-
-ALTER TABLE tb_pessoa
-ADD COLUMN ativo BOOLEAN DEFAULT TRUE;
-
-
