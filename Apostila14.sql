2dd9940b2520bb945e7cf07685a2606daf0ea377
Nathan Lima <nathanfalima189@gmail.com>
2026-05-23 17:08:57 -0300

    Exercicio 1.2 - Trigger DELETE

--- Apostila14.sql
+++ Apostila14.sql
@@ -6,3 +6,34 @@ ALTER TABLE tb_pessoa
 ADD COLUMN ativo BOOLEAN DEFAULT TRUE; 
 
 
+
+
+-- 1.2 Associe um trigger de DELETE à tabela. Quando um DELETE for executado, o trigger
+-- deve atribuir FALSE à coluna ativo das linhas envolvidas. Além disso, o trigger não deve
+-- -- permitir que nenhuma pessoa seja removida.
+
+CREATE OR REPLACE FUNCTION fn_delete_pessoa()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+UPDATE tb_pessoa
+SET ativo = FALSE
+WHERE cod_pessoa = OLD.cod_pessoa;
+
+RETURN NULL;
+
+END;
+$$
+;
+
+CREATE OR REPLACE TRIGGER tg_delete_pessoa
+BEFORE DELETE ON tb_pessoa
+FOR EACH ROW
+EXECUTE PROCEDURE fn_delete_pessoa();
+
+
+
+
+
